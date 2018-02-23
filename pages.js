6b35dce2da700881bde1621a6b60a530547258c2
Daniel GrandeCastelo <sdobrado@gmail.com>
2018-02-22 21:45:19 -0300

    O retorno da api vem sem ssl

--- pages.js
+++ pages.js
@@ -1,10 +1,10 @@
 module.exports = {
   "list": [
-    "https://pt.wikipedia.org/wiki/Luiz_In%C3%A1cio_Lula_da_Silva",
-    "https://pt.wikipedia.org/wiki/Marina_Silva",
-    "https://pt.wikipedia.org/wiki/Jair_Bolsonaro",
-    "https://pt.wikipedia.org/wiki/Robert_Rey",
-    "https://pt.wikipedia.org/wiki/Luciano_Huck",
-    "https://pt.wikipedia.org/wiki/Ciro_Gomes"
+    "http://pt.wikipedia.org/wiki/Luiz_In%C3%A1cio_Lula_da_Silva",
+    "http://pt.wikipedia.org/wiki/Marina_Silva",
+    "http://pt.wikipedia.org/wiki/Jair_Bolsonaro",
+    "http://pt.wikipedia.org/wiki/Robert_Rey",
+    "http://pt.wikipedia.org/wiki/Luciano_Huck",
+    "http://pt.wikipedia.org/wiki/Ciro_Gomes"
   ]
 }
