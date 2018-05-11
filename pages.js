327d65243cce06cedab10bbbe40ec8e249df8151
Daniel Oshiro <daniel0oshiro@gmail.com>
2018-05-11 14:40:12 -0300

    Atualizando pré-candidatos. Fonte: http://politica.estadao.com.br/noticias/eleicoes,veja-quem-quer-ser-presidente-em-2018,70002054149

--- pages.js
+++ pages.js
@@ -1,18 +1,20 @@
 module.exports = {
   'list': [
     'http://pt.wikipedia.org/wiki/Luiz_In%C3%A1cio_Lula_da_Silva',
-    'http://pt.wikipedia.org/wiki/Marina_Silva',
     'http://pt.wikipedia.org/wiki/Jair_Bolsonaro',
-    'http://pt.wikipedia.org/wiki/Robert_Rey',
-    'http://pt.wikipedia.org/wiki/Luciano_Huck',
+    'http://pt.wikipedia.org/wiki/Marina_Silva',
     'http://pt.wikipedia.org/wiki/Ciro_Gomes',
-    'http://pt.wikipedia.org/wiki/Rodrigo_Maia',
     'http://pt.wikipedia.org/wiki/Geraldo_Alckmin',
-    'http://pt.wikipedia.org/wiki/Cristovam_Buarque',
-    'http://pt.wikipedia.org/wiki/Jos%C3%A9_Maria_Eymael',
-    'http://pt.wikipedia.org/wiki/Fernando_Collor_de_Mello',
-    'http://pt.wikipedia.org/wiki/Jo%C3%A3o_Amo%C3%AAdo',
+    'http://pt.wikipedia.org/wiki/Aldo_Rebelo',
     'http://pt.wikipedia.org/wiki/Manuela_d%27%C3%81vila',
-    'https://pt.wikipedia.org/wiki/%C3%81lvaro_Dias'
+    'http://pt.wikipedia.org/wiki/Guilherme_Boulos',
+    'https://pt.wikipedia.org/wiki/%C3%81lvaro_Dias',
+    'http://pt.wikipedia.org/wiki/Rodrigo_Maia',
+    'http://pt.wikipedia.org/wiki/Jo%C3%A3o_Amo%C3%AAdo',
+    'http://pt.wikipedia.org/wiki/Fl%C3%A1vio_Rocha',
+    'http://pt.wikipedia.org/wiki/Henrique_Meirelles',
+    'http://pt.wikipedia.org/wiki/Fernando_Collor_de_Mello',
+    'http://pt.wikipedia.org/wiki/Paulo_Rabello_de_Castro',
+    'http://pt.wikipedia.org/wiki/Levy_Fidelix'
   ]
 }
