75ba34072fc33a7ff4931a8ff180d253ac26bbcb
Daniel GrandeCastelo <sdobrado@gmail.com>
2018-08-10 09:47:45 -0300

    Lista oficial de candidados

--- pages.js
+++ pages.js
@@ -1,20 +1,17 @@
 module.exports = {
-  'list': [
-    'http://pt.wikipedia.org/wiki/Luiz_In%C3%A1cio_Lula_da_Silva',
-    'http://pt.wikipedia.org/wiki/Jair_Bolsonaro',
-    'http://pt.wikipedia.org/wiki/Marina_Silva',
-    'http://pt.wikipedia.org/wiki/Ciro_Gomes',
-    'http://pt.wikipedia.org/wiki/Geraldo_Alckmin',
-    'http://pt.wikipedia.org/wiki/Aldo_Rebelo',
-    'http://pt.wikipedia.org/wiki/Manuela_d%27%C3%81vila',
-    'http://pt.wikipedia.org/wiki/Guilherme_Boulos',
-    'https://pt.wikipedia.org/wiki/%C3%81lvaro_Dias',
-    'http://pt.wikipedia.org/wiki/Rodrigo_Maia',
-    'http://pt.wikipedia.org/wiki/Jo%C3%A3o_Amo%C3%AAdo',
-    'http://pt.wikipedia.org/wiki/Fl%C3%A1vio_Rocha',
-    'http://pt.wikipedia.org/wiki/Henrique_Meirelles',
-    'http://pt.wikipedia.org/wiki/Fernando_Collor_de_Mello',
-    'http://pt.wikipedia.org/wiki/Paulo_Rabello_de_Castro',
-    'http://pt.wikipedia.org/wiki/Levy_Fidelix'
-  ]
+    'list': [
+        'http://pt.wikipedia.org/wiki/Alvaro_Dias',
+        'http://pt.wikipedia.org/wiki/Cabo_Daciolo',
+        'http://pt.wikipedia.org/wiki/Ciro_Gomes',
+        'http://pt.wikipedia.org/wiki/Geraldo_Alckmin',
+        'http://pt.wikipedia.org/wiki/Guilherme_Boulos',
+        'http://pt.wikipedia.org/wiki/Henrique_Meirelles',
+        'http://pt.wikipedia.org/wiki/Jair_Bolsonaro',
+        'http://pt.wikipedia.org/wiki/Jo%C3%A3o_Amo%C3%AAdo',
+        'http://pt.wikipedia.org/wiki/Jo%C3%A3o_Vicente_Goulart',
+        'http://pt.wikipedia.org/wiki/Jos%C3%A9_Maria_Eymael',
+        'http://pt.wikipedia.org/wiki/Luiz_In%C3%A1cio_Lula_da_Silva',
+        'http://pt.wikipedia.org/wiki/Marina_Silva',
+        'http://pt.wikipedia.org/wiki/Vera_L%C3%BAcia_Salgado'
+    ]
 }
